3e13809bd6aaae69cae01dd9b6183e481695b46a
lara <125503675@qq.com>
2017-08-22 13:55:07 +0800

    20170822

---
+++ pages/self_service/self_service.js
@@ -0,0 +1,96 @@
+// pages/self_service/self_service.js
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    searchIconFlag: true,
+    currentTab: 0,
+   
+  },
+  
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    console.log(this.data.searchIconFlag);
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  },
+  bindInput:function(e){
+    var inputLength = e.detail.value.length;
+    if (inputLength){
+      this.setData({ searchIconFlag: false });
+    }else{
+      this.setData({ searchIconFlag: true });
+    }
+  },
+  /** 
+   * 点击tab切换 
+   */
+  swichNav: function (e) {
+    var that = this;
+    if (this.data.currentTab === e.target.dataset.current) {
+      return false;
+    } else {
+      that.setData({
+        currentTab: e.target.dataset.current
+      })
+    }
+  },
+  /** 
+     * 滑动切换tab 
+     */
+  bindChange: function (e) {
+    var that = this;
+    that.setData({ currentTab: e.detail.current });
+  },
+ 
+})
---
+++ pages/test/test.js
@@ -0,0 +1,35 @@
+// Page({
+//   data: {
+//     lastX: 0,  // 按下时的坐标
+//     lastY: 0,
+//     text: "没有滑动",
+//     left:0,   // 元素定位的左边距离
+//     disX: 0  //按下时距元素左边的距离
+//   },
+//   handletouchmove: function (event) {
+//     let currentX = event.touches[0].pageX
+//     var x = currentX - this.data.disX;
+//       this.setData({
+//         left: x +'px'
+//       })
+//   },
+//   handletouchtart: function (event) {
+//     console.log(event)
+//     this.data.lastX = event.touches[0].pageX;
+//     this.data.disX = event.touches[0].pageX - event.target.offsetLeft;
+//     console.log(this.data.disX);
+//   }
+// })
+
+Page({
+  data: {
+    x: 0,
+    y: 0
+  },
+  tap: function (e) {
+    this.setData({
+      x: 30,
+      y: 30
+    });
+  }
+})
