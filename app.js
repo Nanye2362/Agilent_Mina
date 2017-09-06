122803dfffa2f5d778d937e96f60a4e300218594
lara <125503675@qq.com>
2017-09-06 11:02:35 +0800

    lara

--- app.js
+++ app.js
@@ -2,8 +2,6 @@
 var util = require('/utils/util.js');
 App({
   onLaunch: function () {
-    
-    
     //调用API从本地缓存中获取数据
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
--- pages/service_list/service_list.js
+++ pages/service_list/service_list.js
@@ -9,8 +9,12 @@ Page({
     // tab切换  
     currentTab: 0,
     dropdown: false,
-    serviceList: [{ 'slNo': 'SR80109394845432', 'slState': '等待报价确认', 'slReason': 'SR80109394845432', 'slSN': 'RS-PRO-z0290384848', 'slDate': '2017年6月18日09:00 ' }, { 'slNo': 'SR80109394845432', 'slState': '完成确认', 'slReason': 'SR80109394845432', 'slSN': 'RS-PRO-z0290384848', 'slDate': '2017年6月18日09:00 ' }, { 'slNo': 'SR80109394845432', 'slState': '等待报价确认', 'slReason': 'SR80109394845432', 'slSN': 'RS-PRO-z0290384848', 'slDate': '2017年6月18日09:00 ' }, { 'slNo': 'SR80109394845432', 'slState': '完成确认', 'slReason': 'SR80109394845432', 'slSN': 'RS-PRO-z0290384848', 'slDate': '2017年6月18日09:00 ' }]
+    reportFlag: false,
+    serviceListAll: [{ 'slNo': 'SR80109394845432', 'slState': '1', 'slStateDes': '等待报价确认', 'slReason': 'SR80109394845432', 'slSN': 'RS-PRO-z0290384848', 'slDate': '2017年6月18日09:00 ' }, { 'slNo': 'SR80109394845432', 'slState': '1', 'slStateDes': '已安排工程师', 'slReason': 'SR80109394845432', 'slSN': 'RS-PRO-z0290384848', 'slDate': '2017年6月18日09:00 ' }, { 'slNo': 'SR80109394845432', 'slState': '2', 'slStateDes': '我的评价', 'slReason': 'SR80109394845432', 'slSN': 'RS-PRO-z0290384848', 'slDate': '2017年6月18日09:00 ' }, { 'slNo': 'SR80109394845432', 'slState': '3', 'slStateDes': '前往评价', 'slReason': 'SR80109394845432', 'slSN': 'RS-PRO-z0290384848', 'slDate': '2017年6月18日09:00 ' }],
+    serviceListUnderEvaluate:[{}]
+
   },
+
   onLoad: function () {
     var that = this;
     /** 
