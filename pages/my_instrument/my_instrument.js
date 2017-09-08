95c9ac381ed8f72a98a355d1fda3b7e3c37ed8b3
Regina <marynazhao@163.com>
2017-09-08 10:31:00 +0800

    1

--- pages/my_instrument/my_instrument.js
+++ pages/my_instrument/my_instrument.js
@@ -1,4 +1,4 @@
-var common = require("../../utils/common.js");
+var util = require('../../utils/util.js');
 var app = getApp()
 Page({
   data: {
@@ -9,28 +9,63 @@ Page({
     winHeight: 0,
     // tab切换  
     currentTab: 0,
+    displayState: false,
     InstrumentCount: 7,
-    myInstrument: [{ 'product': '气象色谱', 'desc': '490-PRO气相色谱仪', 'seriNo': 'US1727636' }, { 'product': '液相色谱', 'desc': '490-PRO气相色谱仪', 'seriNo': 'US1727675' }]
+    InstrumentList: [{}],
+    ContactGuid:'',
+    ContactId:'',
+    AccountGuid:'',
+    AccountId:'',
   },
+
+  //报修历史
   clickToNext: function(event){
-    common.clickToNext(event);
+    var sn = event.currentTarget.dataset.sn;
+    var contactId = event.currentTarget.dataset.contactid;
+    wx.navigateTo({
+      url: '../service_list/service_list?sn=' + sn + '&contactId=' + contactId,
+    })
   },
+  //报修
+  clickToRepair: function(event){
+    var sn = event.currentTarget.dataset.sn;
+    var contactId = event.currentTarget.dataset.contactid;
+    var contactGuid = event.currentTarget.dataset.contactguid;
+    var accountGuid = event.currentTarget.dataset.accountguid;
+    var accountId = event.currentTarget.dataset.accountid;
+    wx.navigateTo({
+      url: '../confirm_info/confirm_info?sn=' + sn + '&contactId=' + contactId + '&contactGuid=' + contactGuid + '&accountGuid=' + accountGuid + '&accountId=' + accountId,
+    })
+  },
+  //添加仪器
+  /*
+  clickToAdd: function(){
+
+  },*/
+
   onLoad: function () {
     var that = this;
-
-    /** 
-     * 获取系统信息 
-     */
-    wx.getSystemInfo({
-
+    util.NetRequest({
+      url: 'site-mini/my-instrument',
+      data: {
+      },
       success: function (res) {
         that.setData({
-          winWidth: res.windowWidth,
-          winHeight: res.windowHeight
-        });
+          displayState: true,
+        })
+        that.setData({
+          InstrumentCount: res.InstrumentCount,
+          InstrumentList: res.InstrumentList,
+          ContactGuid: res.ContactGuid,
+          ContactId: res.ContactId,
+          AccountGuid: res.AccountGuid,
+          AccountId: res.AccountId,
+        }) 
+      },
+      fail: function (err) {
+        console.log(err);
       }
-
-    });
+    })
   },
   /** 
      * 滑动切换tab 
@@ -56,7 +91,7 @@ Page({
     var that = this;
     var index = event.currentTarget.dataset.index;
     console.log(index);
-    var myInstrument = this.data.myInstrument;
+    var InstrumentList = this.data.InstrumentList;
     wx.showModal({
       title: '提示',
       content: '确定要删除么',
