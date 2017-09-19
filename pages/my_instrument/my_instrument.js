c320f79dca48a8d5957097a72d0748b5370c5faf
lara <125503675@qq.com>
2017-09-19 16:13:38 +0800

    更改PDF打开失败的提示语

--- pages/my_instrument/my_instrument.js
+++ pages/my_instrument/my_instrument.js
@@ -68,7 +68,7 @@ Page({
         } else {
           wx.showModal({
             title: '提示',
-            content: '您暂无报修历史',
+            content: '该仪器暂无报修历史',
             showCancel: false,
             success: function (res) {
               if (res.confirm) {
