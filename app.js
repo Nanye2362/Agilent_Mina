643401b7ad7d353a85e985acc00ce630f8fe1149
chen6850a1 <283255625@qq.com>
2017-09-28 14:50:35 +0800

    新加用户认证后才能通过欢迎页

--- app.js
+++ app.js
@@ -35,17 +35,19 @@ App({
                     success: function (m) {
                       console.log(m);
                       if (m.success == true) {
+                        app.globalData.needCheck = false;
                         wx.setStorageSync('MOBILE', m.mobile);
                         wx.setStorageSync('OPENID', m.openid);
                         app.globalData.isLogin=true;
                         app.gotoIndex();
                       } else {
+                        wx.hideLoading();
+                        app.globalData.needCheck=true;
                         wx.showModal({
                           title: '温馨提示',
                           content: '请先关注安捷伦公众账号',
                           showCancel: false,
                           success: function (res) {
-
                           }
                         })
                         console.log(m.error_msg);
@@ -62,20 +64,60 @@ App({
       }
     });
   },
-  onShow: function () {
-    console.log("index show");
+  onShow: function (res) {
+    var that=this;
+    if (that.globalData.needCheck){
+      wx.getUserInfo({
+        success: function (res) {
+          console.log(res);
+          console.log(wx.getStorageSync('session3rd'));
+          util.NetRequest({
+            url: 'wechat-mini/get-userinfo',
+            data: {
+              encryptedData: res.encryptedData,
+              iv: res.iv,
+              session3rd: wx.getStorageSync('session3rd'),
+              userInfo: JSON.stringify(res.userInfo)
+            },
+            success: function (m) {
+              console.log(m);
+              if (m.success == true) {
+                that.globalData.needCheck = false;
+                wx.setStorageSync('MOBILE', m.mobile);
+                wx.setStorageSync('OPENID', m.openid);
+                that.globalData.isLogin = true;
+                that.gotoIndex();
+              } else {
+                that.globalData.needCheck=true;
+                wx.hideLoading();
+                wx.showModal({
+                  title: '温馨提示',
+                  content: '请先关注安捷伦公众账号',
+                  showCancel: false,
+                  success: function (res) {
+                  }
+                })
+                console.log(m.error_msg);
+              }
+            }
+          })
+        }
+      })
+    }
   },
   globalData: {
     userInfo: null,
     requestList: [],
     isLoading: false,
     isLogin:false,
-    isWelcomeAuth:false
+    isWelcomeAuth:false,
+    needCheck:false
     //token: wx.getStorageSync('token')
   },
   gotoIndex: function () {
     console.log('enter');
-    if (this.globalData.isWelcomeAuth && this.globalData.isLogin){    
+    if (this.globalData.isWelcomeAuth && this.globalData.isLogin){ 
+      this.globalData.isLoading=false;
       wx.switchTab({
         url: '../index/index',
       })
