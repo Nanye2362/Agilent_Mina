210d6430a37d49086e0b9797bf276fd56ee6f8a6
nanye <nanye2362@163.com>
2017-11-03 15:53:00 +0800

    1.11.3版本

--- utils/util.js
+++ utils/util.js
@@ -28,7 +28,7 @@ function NetRequest({ url, data, success, fail, complete, method = "POST" ,showl
     })
   }
   var _csrf = wx.getStorageSync('csrf');
-  var version = "1.11.1";
+  var version = "1.11.3";
   var csrfToken = wx.getStorageSync('csrfCookie')
   if (typeof (data) =='object'){
     data._csrf = _csrf;
