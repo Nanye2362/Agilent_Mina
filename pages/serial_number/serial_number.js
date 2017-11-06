86ef63ba97db8acdf1d165d10cc2e07498ff7ad0
Regina <marynazhao@163.com>
2017-11-06 15:04:12 +0800

    识别

--- pages/serial_number/serial_number.js
+++ pages/serial_number/serial_number.js
@@ -67,8 +67,11 @@ Page({
           filePath: tempFilePaths[0],
           name: 'file',
           success: function (res) {
+			
             console.log(res)
             var value = JSON.parse(res.data).result;
+			      var app = getApp();
+			      app.mta.Event.stat("sn_ocr", { "sn": value });
             // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片  
             if (value){
               _this.setData({
@@ -133,6 +136,7 @@ Page({
   },
 
   clickToSubmit: function (event) {
+	
     var that = this;
     var serNum = this.data.inputValue;
     var contactguid = event.currentTarget.dataset.contactguid;
