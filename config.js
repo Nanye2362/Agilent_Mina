3a2bb53f908c1e08ab4e18e7b4655c5e43347316
Regina <marynazhao@163.com>
2018-09-03 10:01:30 +0800

    服务预约聊天按钮修改为非服务时间弹框

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 var config = {
 
   // 下面的地址配合云端 Server 工作
-  Server: "https://devops.coffeelandcn.cn/",
-  //Server:"https://devopsx.coffeelandcn.cn/", //DEV
+  //Server: "https://devops.coffeelandcn.cn/",//UAT
+  Server:"https://devopsx.coffeelandcn.cn/", //DEV
   //Server: "https://prd.wechat.service.agilent.com/", //PRO
   version:"2.6.15.1",
 
