f5a508b086d0dbd4cd272afa9002ef94f82b1c22
Regina <marynazhao@163.com>
2018-09-20 16:30:52 +0800

    newUI ---myhome

--- config.js
+++ config.js
@@ -3,17 +3,17 @@ var config = {
   En:"PRO",
   // 下面的地址配合云端 Server 工作
   //Server: "https://devops.coffeelandcn.cn/",//UAT
-  //Server:"https://devopsx.coffeelandcn.cn/", //DEV
+  Server:"https://devopsx.coffeelandcn.cn/", //DEV
   //Server: "https://prd.wechat.service.agilent.com/", //PRO
-  Server:"",
+  //Server:"",
   version:"2.9.7.1",
 
 };
 
-if (config.En =="DEV"){
-  config.Server="https://qa.wechat.service.agilent.com/";
-}else{
-  config.Server = "https://prd.wechat.service.agilent.com/";
-}
+// if (config.En =="DEV"){
+//   config.Server="https://qa.wechat.service.agilent.com/";
+// }else{
+//   config.Server = "https://prd.wechat.service.agilent.com/";
+// }
 
 module.exports = config
