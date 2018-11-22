2fe798ab2706e8d16c27caf4aa4951f8da0a4ac3
chenhong <hong.chen5@non.agilent.com>
2018-11-22 09:37:20 +0800

    BQ确认页 文字修改

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 var config = {
 
-  En: "UAT",
+  En: "DEV",
   // 下面的地址配合云端 Server 工作
-  Server: "https://devops.coffeelandcn.cn/",//UAT
+  //Server: "https://devops.coffeelandcn.cn/",//UAT
   //Server:"https://devopsx.coffeelandcn.cn/", //DEV
   //Server: "https://prd.wechat.service.agilent.com/", //PRO
   //Server:"",
-  version: "2.9.7.1",
+  version: "2.11.2.1",
 
 };
 
-// if (config.En =="DEV"){
-//   config.Server="https://qa.wechat.service.agilent.com/";
-// }else{
-//   config.Server = "https://prd.wechat.service.agilent.com/";
-// }
+if (config.En =="DEV"){
+  config.Server="https://qa.wechat.service.agilent.com/";
+}else{
+  config.Server = "https://prd.wechat.service.agilent.com/";
+}
 
 module.exports = config
