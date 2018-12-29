b9e57e9e7f0243d6bd901febb4a2b1961ba0bfde
Sunday233 <1300213959@qq.com>
2018-12-29 13:33:56 +0800

    秒购

--- pages/second-buy/second-buy.js
+++ pages/second-buy/second-buy.js
@@ -1,89 +1,109 @@
 // pages/second-buy/second-buy.js
 var util = require('../../utils/util.js');
 var config = require('../../config.js');
-var dataTemplete={
-  "inputTemplate5": [{ name:"input5_ordersn",nameInfo:"服务单号"}],
-  "inputTemplate2": [{ name: "input2_name", nameInfo: "姓名" }, { name: "input2_tel", nameInfo: "手机" }, { name: "input2_company", nameInfo: "公司" }, { name: "input2_email", nameInfo: "E-Mail"}]
+var dataTemplete = {
+  "inputTemplate5": [{
+    name: "input5_ordersn",
+    nameInfo: "服务单号"
+  }],
+  "inputTemplate2": [{
+    name: "input2_name",
+    nameInfo: "姓名"
+  }, {
+    name: "input2_tel",
+    nameInfo: "手机"
+  }, {
+    name: "input2_company",
+    nameInfo: "公司"
+  }, {
+    name: "input2_email",
+    nameInfo: "E-Mail"
+  }]
 };
 
-var app=getApp();
+var app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-   
+
+    isOnShow: true,
     isScroll: false,
     curIndex: 0,
     toView: 1,
-    scrollH:0,
-    proLink:'',
- 
-    shLoading: false,// 是否显示输入框
-    shInputInfo: false,//  是否显示信息确认页
+    scrollH: 0,
+    proLink: '',
+
+    shLoading: false, // 是否显示输入框
+    shInputInfo: false, //  是否显示信息确认页
     showTemplate: "", //需要显示的输入模块
     hasInputError: false,
     inputError: "",
-    meqiaGroup: "",//美恰分组
-    productid:'',
+    meqiaGroup: "", //美恰分组
+    productid: '',
     //服务秒购咨询
     input2_name: "",
     input2_tel: "",
     input2_company: "",
-    input2_email:"",
+    input2_email: "",
 
     productList: [],
     // meqiaGroup: "T",//美恰分组
     server: config.Server,
-    templete: 'inputTemplate2', 
-    checkfun: 'checkSales', 
+    templete: 'inputTemplate2',
+    checkfun: 'checkSales',
     tapfun: 'salesTap',
     desc: '服务秒购相关事项咨询',
-    leftFix:false,
+    leftFix: false,
     imglist: []
   },
-  
-  onLoad:function(options){
+
+  onShow: function(options) {
+    console.log("onShow");
     var that = this;
-    util.NetRequest({
-      url: 'purchase/get-purchase-list',
-      data: {},
-      success: function (res) {
-        console.log(res); //后台获取到的mycount数据
-        // var fontUrl=res.fontUrl;
-        wx.loadFontFace({
-          family: 'iconfont',
-          source: 'url("//at.alicdn.com/t/' + res.fontUrl+'.woff")',
-          success(res) {
+    if (that.data.isOnShow) {
+      util.NetRequest({
+        url: 'purchase/get-purchase-list',
+        data: {},
+        success: function(res) {
+          console.log(res); //后台获取到的mycount数据
+          // var fontUrl=res.fontUrl;
+          wx.loadFontFace({
+            family: 'iconfont',
+            source: 'url("//at.alicdn.com/t/' + res.fontUrl + '.woff")',
+            success(res) {
 
-          },
-          fail: function (res) {
-            console.log(res);
-          },
-          complete: function (res) {
-            console.log(res.status)
-          }
-        });    
-        that.setData({
-          productList: res.data,
-         
-        })
-      }
-    });
-   
+            },
+            fail: function(res) {
+              console.log(res);
+            },
+            complete: function(res) {
+              console.log(res.status)
+            }
+          });
+          that.setData({
+            productList: res.data,
+
+          })
+        }
+      });
+    };
+    that.data.isOnShow = true;
+  
   },
- onShow:function(){
-   let observer = wx.createIntersectionObserver(this);
-   observer.relativeTo().observe('.top-banner', (res) => {
-     console.log('.top-banner');
-     console.log(res);
-     this.setData({
-       leftFix: res.intersectionRatio > 0 ? false:true
+   onLoad:function(){
+     let observer = wx.createIntersectionObserver(this);
+     observer.relativeTo().observe('.top-banner', (res) => {
+       console.log('.top-banner');
+       console.log(res);
+       this.setData({
+         leftFix: res.intersectionRatio > 0 ? false:true
+       })
      })
-   })
 
- },
+   },
   // 获取右栏每块高度
   // onShow:function(){
   //   let queryRight=wx.createSelectorQuery().in(this);
@@ -126,12 +146,12 @@ Page({
   //       scrollH: scrollTop
   //     })
   //   },0)
-   
-    // setTimeout(function () {
-    // wx.pageScrollTo({
-    //   scrollTop: scrollTop,
-    //   duration: 50
-    // })},200)
+
+  // setTimeout(function () {
+  // wx.pageScrollTo({
+  //   scrollTop: scrollTop,
+  //   duration: 50
+  // })},200)
   //   var animation = wx.createAnimation({
   //     duration: 5000,
   //     timingFunction: 'ease',
@@ -151,7 +171,7 @@ Page({
   //         ani1: animation.export(),
   //         ani21: animation2.export()
   //       })
-    
+
   //   } else {
   //     animation.opacity(1).translate(0, 0).step()
   //     animation2.opacity(1).translate(0, 0).step()
@@ -161,7 +181,7 @@ Page({
   //         ani2: animation.export(),
   //         ani22: animation2.export(),
   //       })
-     
+
   // }
   // },
   // onScroll:function(e){
@@ -177,12 +197,12 @@ Page({
   //       if(scrollTop>=0&&scrollTop<scrollArr[0]){
   //         this.setData({
   //           curIndex:0
-            
+
   //         })
   //       }else if(scrollTop>=scrollArr[i-1]&&scrollTop<scrollArr[i]){
   //         this.setData({
   //           curIndex: i
-            
+
   //         })
   //       }
   //     }
@@ -191,7 +211,7 @@ Page({
   // onPageScroll: function (e) { // 获取滚动条当前位置
   //   console.log("PageScroll");
   //   console.log(e);
- 
+
   //   var leftFix=this.data.leftFix;
   //   var that = this;
   //   if (e.scrollTop > 154) {
@@ -206,36 +226,36 @@ Page({
   //     })
   //     console.log(leftFix);
   //   }
-    // var that = this;
-    // //当滚动的top值最大或者最小时，为什么要做这一步是由于在手机实测小程序的时候会发生滚动条回弹，所以为了解决回弹，设置默认最大最小值   
-    // if (e.scrollTop <= 0) {
-    //  // e.scrollTop = 0;
-    // } else if (e.scrollTop > wx.getSystemInfoSync().windowHeight) {
-    //  //e.scrollTop = wx.getSystemInfoSync().windowHeight;
-    // }
-    // //判断浏览器滚动条上下滚动   
-    // if (e.scrollTop > this.data.scrollH || e.scrollTop == wx.getSystemInfoSync().windowHeight) {
-    //   that.setData({
-    //     bannerShow: false
-    //   })
-    //   console.log('向下滚动');
-    // } else{
-    //   that.setData({
-    //     bannerShow: true
-    //   })
-    //   console.log('向上滚动');
-    // }
+  // var that = this;
+  // //当滚动的top值最大或者最小时，为什么要做这一步是由于在手机实测小程序的时候会发生滚动条回弹，所以为了解决回弹，设置默认最大最小值   
+  // if (e.scrollTop <= 0) {
+  //  // e.scrollTop = 0;
+  // } else if (e.scrollTop > wx.getSystemInfoSync().windowHeight) {
+  //  //e.scrollTop = wx.getSystemInfoSync().windowHeight;
+  // }
+  // //判断浏览器滚动条上下滚动   
+  // if (e.scrollTop > this.data.scrollH || e.scrollTop == wx.getSystemInfoSync().windowHeight) {
+  //   that.setData({
+  //     bannerShow: false
+  //   })
+  //   console.log('向下滚动');
+  // } else{
+  //   that.setData({
+  //     bannerShow: true
+  //   })
+  //   console.log('向上滚动');
+  // }
 
-    //给scrollTop重新赋值    
-    // setTimeout(function () {
-    //   that.setData({
-    //     scrollH: e.scrollTop
-    //   })
-    // }, 0)
+  //给scrollTop重新赋值    
+  // setTimeout(function () {
+  //   that.setData({
+  //     scrollH: e.scrollTop
+  //   })
+  // }, 0)
 
   // },
-// 左侧导航栏切换
-  switchTab: function (e) {
+  // 左侧导航栏切换
+  switchTab: function(e) {
     console.log("switchTab");
     console.log(e);
     var toView = this.data.toView;
@@ -246,46 +266,49 @@ Page({
     //   isScroll: true
     // })
     // setTimeout(function () {
-      that.setData({
-        curIndex: e.currentTarget.dataset.current,
-        toView: e.currentTarget.dataset.id,
-      })
+    that.setData({
+      curIndex: e.currentTarget.dataset.current,
+      toView: e.currentTarget.dataset.id,
+    })
     // }, 0);
     // setTimeout(function () {
     //   that.setData({
     //     isScroll: false
     //   })
     // }, 1);
-   
+
   },
 
-//美恰联系
-  startChat: function (e) {
+  //美恰联系
+  startChat: function(e) {
     var app = getApp();
     // console.log("productid");
     // console.log(this.data.productid);
     // console.log("product");
     // console.log(this.data.productname);
-    app.mta.Event.stat("second_buy", { "product": this.data.productname, "productid": this.data.productid});
+    app.mta.Event.stat("second_buy", {
+      "product": this.data.productname,
+      "productid": this.data.productid
+    });
     this.setData({
-      meqiaGroup:this.data.meqiaGroup
+      meqiaGroup: this.data.meqiaGroup
     });
   },
   // 点击联系客服事件
 
-  showInputPanel: function (e) {// 点击弹出信息输入
+  showInputPanel: function(e) { // 点击弹出信息输入
     console.log('showinputpanel');
-   
+
 
     var that = this;
     // 检测工作时间
-    util.checkWorktime(function () {
+    util.checkWorktime(function() {
       switch (e.currentTarget.dataset.tapfun) {
         case "salesTap":
           that.salesTap(e);
           break;
       }
-    }, function () {
+    }, function() {
       console.log(e)
       switch (e.currentTarget.dataset.tapfun) {
         case "salesTap":
@@ -296,7 +319,7 @@ Page({
 
   },
   // 工作时间正式执行的函数
-  salesTap: function (e) {
+  salesTap: function(e) {
     console.log(e);
     var that = this;
     that.setData({
@@ -307,11 +330,11 @@ Page({
       productid: e.currentTarget.dataset.productid,
       productname: e.currentTarget.dataset.productname
     })
-    
+
     // 工作时间成功后验证是否认证过
-    util.IsCertificate(function () {
+    util.IsCertificate(function() {
       that.getCusInfo();
-    }, function () {
+    }, function() {
       var userInputInfo = wx.getStorageSync('UserInputCache_ol');
       console.log(userInputInfo);
       if (userInputInfo) {
@@ -324,19 +347,19 @@ Page({
     })
   },
   // 验证过的，信息从后台取出
-  getCusInfo: function (e) {
+  getCusInfo: function(e) {
     var that = this;
     util.NetRequest({
       url: 'site-mini/meqia-getuserinfo',
-      success: function (res) {
+      success: function(res) {
         console.log(res);
         that.setData({
           input2_name: res.userinfo.name,
           input2_tel: res.userinfo.tel,
           input2_company: res.userinfo.meta.company,
-          input2_email:res.userinfo.meta.email
+          input2_email: res.userinfo.meta.email
         })
-          // 认证过直接显示确认信息
+        // 认证过直接显示确认信息
         var showInfo = "";
         for (var i in dataTemplete[that.data.showTemplate]) {
           showInfo += dataTemplete[that.data.showTemplate][i]["nameInfo"] + "：" + that.data[dataTemplete[that.data.showTemplate][i]["name"]] + "\r\n";
@@ -351,7 +374,7 @@ Page({
     })
   },
   // 未认证用户输入信息为空时弹出的提示
-  checkSales: function () {
+  checkSales: function() {
     if (this.data.input2_name.length == 0) {
       this.setData({
         hasInputError: true,
@@ -393,7 +416,7 @@ Page({
     //   return false;
     // }
 
-  
+
 
     this.setData({
       hasInputError: false,
@@ -403,18 +426,18 @@ Page({
     return true;
   },
 
-  checkEmail:function(str){
+  checkEmail: function(str) {
     var reg = /^(\w)+(\.\w+)*@(\w)+((\.\w+)+)$/;
     return reg.test(str);
   },
   //右上角关闭提示框
-  closeShLoading: function () {
+  closeShLoading: function() {
     this.setData({
       shInputInfo: false,
     })
   },
   // 点击确认
-  getInputInfo: function () {
+  getInputInfo: function() {
     console.log(this.data.checkFun);
     var hasError = false;
     switch (this.data.checkFun) {
@@ -422,18 +445,24 @@ Page({
         hasError = !this.checkSales();
         break;
     }
-    var dataObj = { "type": this.data.showTemplate };
+    var dataObj = {
+      "type": this.data.showTemplate
+    };
     var showInfo = "";
     for (var i in dataTemplete[this.data.showTemplate]) {
       dataObj[dataTemplete[this.data.showTemplate][i]["name"]] = this.data[dataTemplete[this.data.showTemplate][i]["name"]];
       showInfo += dataTemplete[this.data.showTemplate][i]["nameInfo"] + "：" + this.data[dataTemplete[this.data.showTemplate][i]["name"]] + "\r\n";
     }
-    return { hasError: hasError, showInfo: showInfo, obj: dataObj }
+    return {
+      hasError: hasError,
+      showInfo: showInfo,
+      obj: dataObj
+    }
   },
   // 未认证，将用户输入数据传给后台
-  okTap: function (e) {
+  okTap: function(e) {
     var returnObj = this.getInputInfo();
-    var email=this.data.input2_email;
+    var email = this.data.input2_email;
     if (returnObj.hasError) {
       return false;
     }
@@ -442,12 +471,15 @@ Page({
       url: 'site-mini/meqia-postdata',
       data: {
         'info': JSON.stringify(returnObj.obj),
-        'email':email// 多传一个参数email
+        'email': email // 多传一个参数email
       },
-      success: function (res) {
+      success: function(res) {
         console.log(res);
-        if (returnObj.obj.type == 'inputTemplate2') {//缓存用户模板2输入的信息
-          var userInputInfo = { inputInfo: returnObj.obj, sessionId: res.id };
+        if (returnObj.obj.type == 'inputTemplate2') { //缓存用户模板2输入的信息
+          var userInputInfo = {
+            inputInfo: returnObj.obj,
+            sessionId: res.id
+          };
           wx.setStorage({
             'key': 'UserInputCache_ol',
             'data': userInputInfo
@@ -463,68 +495,78 @@ Page({
       }
     })
   },
-  cancelTap: function () {
+  cancelTap: function() {
     this.setData({
       shLoading: false,
       hasInputError: false,
       inputError: "",
-    
+
     })
   },
   // 输入信息修改
-  infoCancelTap: function () {
+  infoCancelTap: function() {
     this.setData({
       shInputInfo: false,
       shLoading: true
     })
   },
-  bindKeyInput: function (e) {
+  bindKeyInput: function(e) {
     var obj = {};
     obj[e.currentTarget.dataset.name] = e.detail.value;
     this.setData(obj);
   },
   // 非工作时间的弹出框内容
-  showOfflineText: function () {
+  showOfflineText: function() {
     wx.showModal({
       title: '温馨提示',
       content: '感谢您一直以来对我们工作的关注和支持。我们的工作时间是周一至周五的 8:30-17:30，双休日（除节假日外）仅提供紧急电话技术支持，服务时间为：9:00-17:00。',
       showCancel: false,
-      success: function (res) {
-      }
+      success: function(res) {}
     })
   },
   /*
    跳转图片html
   */
-  skipHtml5Page: function (e) {
+  skipHtml5Page: function(e) {
     console.log(e);
-    var proLink=e.currentTarget.dataset.imglink;
-    wx.setStorage({
-      key: "openHtmlUrl",
-      data: proLink ,
-      success: function () {
-        wx.navigateTo({
-          url: '../html/openHtml',
-        });
-      }
-    })
+    var proLink = e.currentTarget.dataset.imglink;
+    if (proLink.length!=0){
+      wx.setStorage({
+        key: "openHtmlUrl",
+        data: proLink,
+        success: function () {
+          wx.navigateTo({
+            url: '../html/openHtml',
+          });
+        }
+      })
+    }
+    
   },
-  previewImage:function(e){
+  previewImage: function(e) {
     console.log(e);
+
     var current = e.currentTarget.dataset.src;
     console.log(current);
-    var imglist=this.data.imglist;
+    var imglist = this.data.imglist;
     imglist.push(current);
+    var isOnShow = this.data.isOnShow;
+    var that = this;
     console.log(imglist)
     wx.previewImage({
       current: current, // 当前显示图片的http链接  
-      urls: imglist // 需要预览的图片http链接列表  
+      urls: imglist, // 需要预览的图片http链接列表  
+      success: function(res) {
+        that.setData({
+          isOnShow: false
+        })
+      }
     })
   },
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
