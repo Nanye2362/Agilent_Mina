d3bda0a17ab8ad72e06a029c0dd94e5936cfe600
Sunday233 <1300213959@qq.com>
2019-01-24 15:18:41 +0800

    config.js

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ var config = {
   En: "DEV",
   // 下面的地址配合云端 Server 工作
   //Server: "https://devops.coffeelandcn.cn/",//UAT
+  //https://qa.wechat.service.agilent.com/
   //Server:"https://devopsx.coffeelandcn.cn/", //DEV
   //Server: "https://prd.wechat.service.agilent.com/", //PRO
   //Server:"",
@@ -11,7 +12,7 @@ var config = {
 };
 
 if (config.En =="DEV"){
-  config.Server="https://qa.wechat.service.agilent.com/";
+  config.Server ="https://devops.coffeelandcn.cn/";
 }else{
   config.Server = "https://prd.wechat.service.agilent.com/";
 }
