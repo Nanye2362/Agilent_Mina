0dd388192e55be4dcee26ba7cdfa64bb9df188a2
sky <595931496@qq.com>
2019-08-22 14:58:45 +0800

    轮播和dev授权配置修改

--- pages/user_guidelines/user_guidelines.js
+++ pages/user_guidelines/user_guidelines.js
@@ -20,9 +20,9 @@ Page({
 
     var url="";
     if (config.En=="DEV"){
-      url ="https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxbed9f0dd30870bab&redirect_uri=https%3a%2f%2fdevops.coffeelandcn.cn%2fsite%2fuser-guidelines&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect";
+      url ="https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxbed9f0dd30870bab&redirect_uri=https%3a%2f%2fqa.wechat.service.agilent.com%2fsite%2fuser-guidelines&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect";
     }else{
-      url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxf939d5b301703b2f&redirect_uri=https%3a%2f%2fprd.wechat.service.agilent.com%2fsite%2fuser-guidelines&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect"
+      url ="https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxf939d5b301703b2f&redirect_uri=https%3a%2f%2fprd.wechat.service.agilent.com%2fsite%2fuser-guidelines&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect"
     }
 
     this.setData({
@@ -30,26 +30,26 @@ Page({
     });
     //腾讯mat统计结束
   },
-  
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
@@ -64,20 +64,20 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   },
 })
