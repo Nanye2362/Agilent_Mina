16309fd7bbd5dbf7ca1f0c0ec37c3db7ae0eba80
sky <595931496@qq.com>
2020-12-14 15:36:26 +0800

    离线消息url解码,图片压缩

--- pages/sobot_html/openHtml.js
+++ pages/sobot_html/openHtml.js
@@ -12,15 +12,21 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var that=this;
-    wx.getStorage({
-      key: 'sobotHtmlUrl',
-      success: function (res) {
-        that.setData({
-          openHtmlUrl:res.data
-        })
-      }
+
+    this.setData({
+      openHtmlUrl:decodeURIComponent(options.url)
+     // openHtmlUrl:decodeURIComponent(url)
     })
+
+    // var that=this;
+    // wx.getStorage({
+    //   key: 'sobotHtmlUrl',
+    //   success: function (res) {
+    //     that.setData({
+    //       openHtmlUrl:res.data
+    //     })
+    //   }
+    // })
   },
 
   /**
